17157956a1368154a2d9c49d7c086a03e84e0e24
Heruanam <85220409+Heruanam20@users.noreply.github.com>
2024-06-10 21:39:22 -0700

    ethereum.sh

--- ethereum.sh
+++ ethereum.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://eth1.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://eth1.lava.build/lava-referer-368256d2-91e5-4872-8077-381483d885b1/"
 
 OUTPUT_DIR="logs"
 
